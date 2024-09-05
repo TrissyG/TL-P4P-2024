d987e28a5c13ef64d93ab5acef8a3a0adad3b5ed
TrissyG <tmon261@aucklanduni.ac.nz>
2024-09-05 13:42:36 +1200

    locationing 11: added AudioSourceManagement namespace to SpectatorUI, added arrows for angle definitions

--- ProjectSource/VR-UI-controls/Assets/Scripts/Polar.cs
+++ ProjectSource/VR-UI-controls/Assets/Scripts/Polar.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
 
-public class Polar
-{
-    public float radius, inclination, azimuth;
+// public class Polar : MonoBehaviour
+// {
+//     public float radius, inclination, azimuth;
 
-    public static Vector3 ToCartesian(float radius, float inclination, float azimuth)
-    {
-        float x = radius * Mathf.Sin(inclination * Mathf.Deg2Rad) * Mathf.Cos(azimuth * Mathf.Deg2Rad);
-        float y = radius * Mathf.Cos(inclination * Mathf.Deg2Rad);
-        float z = radius * Mathf.Sin(inclination * Mathf.Deg2Rad) * Mathf.Sin(azimuth * Mathf.Deg2Rad);
-        return new Vector3(x, y, z);
-    }
+//     public static Vector3 ToCartesian(float radius, float inclination, float azimuth)
+//     {
+//         float x = radius * Mathf.Sin(inclination * Mathf.Deg2Rad) * Mathf.Cos(azimuth * Mathf.Deg2Rad);
+//         float y = radius * Mathf.Cos(inclination * Mathf.Deg2Rad);
+//         float z = radius * Mathf.Sin(inclination * Mathf.Deg2Rad) * Mathf.Sin(azimuth * Mathf.Deg2Rad);
+//         return new Vector3(x, y, z);
+//     }
 
-    public Polar(float radius, float inclination, float azimuth)
-    {
-        this.radius = radius;
-        this.inclination = inclination;
-        this.azimuth = azimuth;
-    }
+//     public Polar(float radius, float inclination, float azimuth)
+//     {
+//         this.radius = radius;
+//         this.inclination = inclination;
+//         this.azimuth = azimuth;
+//     }
 
-    public Vector3 ToCartesian()
-    {
-        return Polar.ToCartesian(radius, inclination, azimuth);
-    }
-}
+//     public Vector3 ToCartesian()
+//     {
+//         return Polar.ToCartesian(radius, inclination, azimuth);
+//     }
+// }
--- ProjectSource/VR-UI-controls/Assets/Scripts/SpectatorUI.cs
+++ ProjectSource/VR-UI-controls/Assets/Scripts/SpectatorUI.cs
@@ -5,6 +5,7 @@ using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 using System;
+using AudioSourceManagement;
 
 public class SpectatorUI : MonoBehaviour
 {
@@ -76,6 +77,8 @@ public class SpectatorUI : MonoBehaviour
     // audio source to control
     public GameObject radioPolygon; // the rendered radio object 'Radio'
     public GameObject radio; // the 'Audio Source' GameObject child of the rendered object
+
+    private AudioSourceManager audioSourceManager;
     private MeshRenderer radioAudioSourceRenderer; // if we want to toggle visibility of the audio source
     private AudioSource radioAudioSource;
     
@@ -1173,6 +1176,7 @@ public class SpectatorUI : MonoBehaviour
         }
     }
 
+    // Tracks the radius slider steps
     private float GetNearestRadiusStep(float value)
     {
         float nearestStep = steps[0];
@@ -1189,6 +1193,11 @@ public class SpectatorUI : MonoBehaviour
         }
 
         return nearestStep;
+    }
+
+    private void setSoundOffset(float radius, float inclination, float azimuth)
+    {
+        audioSourceManager.SetSphericalCoordinates(radius, inclination, azimuth);
     }
 
     private void ExitApplication(ClickEvent evt)
@@ -1209,4 +1218,78 @@ public class SpectatorUI : MonoBehaviour
     }
 
 
+}
+
+namespace AudioSourceManagement {
+    public class AudioSourceManager : MonoBehaviour
+    {   
+        public GameObject radioPolygon; // the rendered radio object 'Radio', around which the sound object will be placed
+        public GameObject radio; // the 'Audio Source' GameObject child of the rendered object, to be offset
+        public AudioSource audioSource;
+        private Polar polarOffset; // (radius, inclination, azimuth) of the sound object relative to the radioPolygon
+
+        private void Start()
+        {   
+            radioPolygon = GameObject.Find("Radio");
+            radio = radioPolygon.transform.Find("Audio Source").gameObject;
+            audioSource = radio.GetComponent<AudioSource>();
+
+            if (radio != null)
+            {
+                Debug.LogWarning("No GameObject found for radio");
+                return;
+            }
+            polarOffset = new Polar(0f, 0f, 0f);  // Initialize with some default values
+            
+            // Ensure soundObject and parentObject are set before updating position
+            if (radioPolygon != null && radio != null)
+            {
+                UpdateSoundObjectPosition();
+            }
+            else
+            {
+                Debug.LogWarning("soundObject or parentObject is not assigned.");
+            }
+        }
+
+        public void SetSphericalCoordinates(float radius, float inclination, float azimuth)
+        {
+            polarOffset.radius = radius;
+            polarOffset.inclination = inclination;
+            polarOffset.azimuth = azimuth;
+            UpdateSoundObjectPosition();
+        }
+
+        private void UpdateSoundObjectPosition()
+        {
+            Vector3 offset = polarOffset.ToCartesian();
+            radio.transform.position = radioPolygon.transform.position + offset;
+        }
+    }
+
+    
+    public class Polar : MonoBehaviour
+    {
+        public float radius, inclination, azimuth;
+
+        public static Vector3 ToCartesian(float radius, float inclination, float azimuth)
+        {
+            float x = radius * Mathf.Sin(inclination * Mathf.Deg2Rad) * Mathf.Cos(azimuth * Mathf.Deg2Rad);
+            float y = radius * Mathf.Cos(inclination * Mathf.Deg2Rad);
+            float z = radius * Mathf.Sin(inclination * Mathf.Deg2Rad) * Mathf.Sin(azimuth * Mathf.Deg2Rad);
+            return new Vector3(x, y, z);
+        }
+
+        public Polar(float radius, float inclination, float azimuth)
+        {
+            this.radius = radius;
+            this.inclination = inclination;
+            this.azimuth = azimuth;
+        }
+
+        public Vector3 ToCartesian()
+        {
+            return Polar.ToCartesian(radius, inclination, azimuth);
+        }
+    }
 }
