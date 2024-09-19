b6f272436a0f51ae10991d2c2fa5006a2faa68ac
Luca <77914022+leas022@users.noreply.github.com>
2024-09-20 07:34:12 +1200

    locationing 22.2: DataLoggingManager

--- ProjectSource/VR-UI-controls/Assets/Scripts/DataLoggingManager.cs
+++ ProjectSource/VR-UI-controls/Assets/Scripts/DataLoggingManager.cs
@@ -15,14 +15,37 @@ public class DataLoggingManager : MonoBehaviour
     private string ratingTabletPath;
     private string cameraPath;
     private string radioPath;
+
+    private string locationingPath;
+
     private bool grabbedRatingTablet;
     private bool grabbedRadio;
     private bool headlocked;
+    // LocationingMode data captured
+    private bool activatedLocationingMode; // from SpectatorUI
+
+    private int RadioPositionIndex; // from SpectatorUI
+    private bool RadioVisible = true; // from SpectatorUI
+    private bool AudioSourceVisible;// from SpectatorUI
+    private float offsetRadius; // from SpectatorUI
+    private float offsetAzimuth; // from SpectatorUI
+    private float offsetInclination; // from SpectatorUI
+    private bool pressedLocationingButton; // from AimingDistanceManager
+    private Vector3 audioSourceLocation; // from AimingDistanceManager
+    private Vector3 rayOriginPoint; // from AimingDistanceManager
+    private Vector3 rayDirection; // from AimingDistanceManager
+    private float displacementAngleFromAudioSource; // from AimingDistanceManager
+    private float displacementAngleFromRadio; // from AimingDistanceManager
+    private int OffsetPresetIndex; // from AimingDistanceManager
+
 
     private enum ObjectName
     {
         Radio,
         RatingTablet,
+        RightHandController,
+        SpectatorUI
+
     }
 
     // Start is called before the first frame update
@@ -30,7 +53,7 @@ public class DataLoggingManager : MonoBehaviour
     {
         radio = GameObject.Find("Radio");
         camera = Camera.main.transform;
-        
+
         initiateCsvFile();
 
         // calls logPositions every half second
@@ -91,6 +114,26 @@ public class DataLoggingManager : MonoBehaviour
             cameraWriter.Flush();
             cameraWriter.Close();
         }
+
+        if (activatedLocationingMode && pressedLocationingButton)
+        {
+            LogLocationingData();
+            pressedLocationingButton = false;
+        }
+    }
+
+    public void LogLocationingData()
+    {
+        DateTime time = DateTime.Now;
+        Vector3 radioPosition = radio.transform.position;
+        Quaternion radioRotation = radio.transform.rotation;
+        using (StreamWriter writer = new StreamWriter(locationingPath, true))
+        {
+            string lineToWrite = $"{SceneManager.GetActiveScene().name},{activatedLocationingMode},{pressedLocationingButton},{OffsetPresetIndex},{RadioPositionIndex},{RadioVisible},{AudioSourceVisible},{offsetRadius},{offsetAzimuth},{offsetInclination},{displacementAngleFromAudioSource},{displacementAngleFromRadio},{audioSourceLocation.x},{audioSourceLocation.y},{audioSourceLocation.z},{rayOriginPoint.x},{rayOriginPoint.y},{rayOriginPoint.z},{rayDirection.x},{rayDirection.y},{rayDirection.z},{radioPosition.x},{radioPosition.y},{radioPosition.z},{time}";
+            writer.WriteLine(lineToWrite);
+            writer.Flush();
+            writer.Close();
+        }
     }
 
     private void writeEventToFile(string eventName, ObjectName objectName)
@@ -102,7 +145,8 @@ public class DataLoggingManager : MonoBehaviour
         if (objectName == ObjectName.Radio)
         {
             position = camera.InverseTransformPoint(radio.transform.position);
-        } else if (objectName == ObjectName.RatingTablet)
+        }
+        else if (objectName == ObjectName.RatingTablet)
         {
             position = camera.InverseTransformPoint(ratingTablet.transform.position);
         }
@@ -133,6 +177,7 @@ public class DataLoggingManager : MonoBehaviour
         ratingTabletPath = Path.Combine(LOGGING_DIRECTORY, "rating-tablet-" + ExperimentData.ExperimentStartTime + ".csv");
         radioPath = Path.Combine(LOGGING_DIRECTORY, "radio-" + ExperimentData.ExperimentStartTime + ".csv");
         cameraPath = Path.Combine(LOGGING_DIRECTORY, "camera-" + ExperimentData.ExperimentStartTime + ".csv");
+        locationingPath = Path.Combine(LOGGING_DIRECTORY, "locationing-" + ExperimentData.ExperimentStartTime + ".csv");
 
         if (!ExperimentData.loggingFileHeader)
         {
@@ -171,6 +216,15 @@ public class DataLoggingManager : MonoBehaviour
                 cameraWriter.Flush();
                 cameraWriter.Close();
             }
+
+            using (StreamWriter locationingWriter = new StreamWriter(locationingPath, true))
+            {
+                string lineToWrite = "Scenario,LocationingMode,PressedLocationingButton,OffsetPresetIndex,RadioPositionIndex,RadioVisible,AudioSourceVisible,offsetRadius,offsetAzimuth,offsetInclination,displacementAngleAS,displacementAngleR,AudioSourcePositionX,AudioSourcePositionY,AudioSourcePositionZ,rayOriginX,rayOriginY,rayOriginZ,rayDirectionX,rayDirectionY,rayDirectionZ,radioPositionX,radioPositionY,radioPositionZ,Time";
+
+                locationingWriter.WriteLine(lineToWrite);
+                locationingWriter.Flush();
+                locationingWriter.Close();
+            }
             ExperimentData.loggingFileHeader = true;
         }
 
@@ -206,7 +260,84 @@ public class DataLoggingManager : MonoBehaviour
         writeEventToFile("grabExited", ObjectName.RatingTablet);
     }
     // // locationing mode - when the user aims and clicks the x button, record the distance between the radio and 
-    // public void {
+    public void pressLocationingButton()
+    {
+        pressedLocationingButton = true;
+        writeEventToFile("pressLocationingButton", ObjectName.RightHandController);
+    }
 
-    // }
+    public void releaseLocationingButton()
+    {
+        pressedLocationingButton = false;
+        writeEventToFile("releaseLocationingButton", ObjectName.RightHandController);
+    }
+
+    public void activateLocationingMode()
+    {
+        activatedLocationingMode = true;
+        writeEventToFile("activateLocationingMode", ObjectName.SpectatorUI);
+    }
+
+    public void deactivateLocationingMode()
+    {
+        activatedLocationingMode = false;
+        writeEventToFile("deactivateLocationingMode", ObjectName.SpectatorUI);
+    }
+
+    public void setRadioPositionIndex(int index)
+    {
+        RadioPositionIndex = index;
+    }
+
+    public void setRadioVisible(bool visible)
+    {
+        RadioVisible = visible;
+    }
+
+    public void setAudioSourceVisible(bool visible)
+    {
+        AudioSourceVisible = visible;
+    }
+
+    public void setAudioSourceOffset(float radius, float azimuth, float inclination)
+    {
+        offsetRadius = radius;
+        offsetAzimuth = azimuth;
+        offsetInclination = inclination;
+    }
+
+    public void setAudioSourceLocation(Vector3 location)
+    {
+        audioSourceLocation = location;
+    }
+
+    public void setRayOriginPoint(Vector3 point)
+    {
+        rayOriginPoint = point;
+    }
+
+    public void setRayDirection(Vector3 direction)
+    {
+        rayDirection = direction;
+    }
+
+
+    public void setDisplacementAngleFromAudioSource(float angle)
+    {
+        displacementAngleFromAudioSource = angle;
+    }
+
+    public void setDisplacementAngleFromRadio(float angle)
+    {
+        displacementAngleFromRadio = angle;
+    }
+
+    public void setOffsetPreset(int index)
+    {
+        OffsetPresetIndex = index;
+    }
+
+
+
+
 }
